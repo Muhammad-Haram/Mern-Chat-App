cba01992e2eb12f34862abaa3734edbfe5bd6d30
Muhammad Haram <haramh643@gmail.com>
2024-11-15 02:05:59 +0500

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,7 @@
 import express from "express";
-import dotenv from "dotenv"; 
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import { app,server } from "./socket/socket.js";
+import { app, server } from "./socket/socket.js";
 dotenv.config({});
 
- 
-const PORT = process.env.PORT || 5000;
