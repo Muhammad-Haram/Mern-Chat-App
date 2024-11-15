b0fa50c96abfb6de2d4e2d36df50fd0142e4c5c7
Muhammad-Haram <haramh643@gmail.com>
2024-11-15 18:16:42 +0500

    connect mongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import { app, server } from "./socket/socket.js";
+import connectDB from "./config/database.js";
 dotenv.config({});
 
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`server listen at port ${PORT}`);
+});
