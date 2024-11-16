671d97bfd9d1d45b14a486b664cf4b4ecb887a67
Muhammad-Haram <haramh643@gmail.com>
2024-11-16 13:38:44 +0500

    register logic

---
+++ backend/controllers/userController.js
@@ -0,0 +1,38 @@
+import { User } from "../models/userModels";
+import bcryptjs from "bcryptjs";
+
+export const register = async (req, res) => {
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+    if (!fullName || !username || !password || !confirmPassword || !gender) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ message: "Password not matched" });
+    }
+
+    const user = await User.findOne({ username });
+    if (user) {
+      return res
+        .status(400)
+        .json({ message: "username already exsist try different" });
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+
+    //profilephotos
+    const maleProfilePhoto = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const femaleProfilePhoto = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    await User.create({
+      fullName,
+      username,
+      password: hashedPassword,
+      profilePhoto: gender === male ? maleProfilePhoto : femaleProfilePhoto,
+      gender,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
