e7feeb9b8afd71b116c613d987cbd03699b9333e
Muhammad-Haram <haramh643@gmail.com>
2024-11-20 13:55:35 +0500

    create components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import HomePage from './components/HomePage'
+import Signup from './components/Signup'
+import Login from './components/Login'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />
+  },
+  {
+    path: "/signup",
+    element: <Signup />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+
+])
 
 const App = () => {
   return (
-    <div>
-      hi
+    <div className='p-4 h-screen flex items-center justify-center'>
+      <RouterProvider router={router} />
     </div>
   )
 }
