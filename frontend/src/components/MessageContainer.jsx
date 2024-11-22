958699132ebd986e8145e6aae632a8a6d847c2ac
Muhammad-Haram <haramh643@gmail.com>
2024-11-22 15:02:39 +0500

    add components

---
+++ frontend/src/components/MessageContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MessageContainer = () => {
+    return (
+        <div>
+            message container
+        </div>
+    )
+}
+
+export default MessageContainer
