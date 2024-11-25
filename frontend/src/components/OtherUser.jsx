ae38416f4ad3177a4a51a33a9aece9debfb487dc
Muhammad-Haram <haramh643@gmail.com>
2024-11-25 12:46:22 +0500

    get other users and logout

--- frontend/src/components/OtherUser.jsx
+++ frontend/src/components/OtherUser.jsx
@@ -1,26 +1,24 @@
 import React from 'react'
 
-const OtherUser = () => {
+const OtherUser = ({ user }) => {
     return (
         <>
-
             <div className={`flex gap-2 items-center text-white hover:bg-zinc-900 hover:text-zinc-200 rounded p-2 cursor-pointer`}>
                 <div className={`avatar online`}>
                     <div className='w-12 rounded-full'>
-                        <img src={"https://avatar.iran.liara.run/public/boy?username=haram123"} alt="user-profile" />
+                        <img src={user?.profilePhoto} alt="user-profile" />
                     </div>
                 </div>
 
                 <div className='flex flex-col flex-1'>
                     <div className='flex justify-between gap-2'>
-                        <p>haram</p>
+                        <p>{user?.fullName}</p>
                     </div>
                 </div>
 
             </div>
 
             <div className='divider my-0 py-0 h-1'></div>
-
         </>
     )
 }
