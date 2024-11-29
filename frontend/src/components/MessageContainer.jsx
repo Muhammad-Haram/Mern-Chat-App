c0c70a7e751fea1644c286f7718d36e7bfd67ccf
Muhammad-Haram <haramh643@gmail.com>
2024-11-29 12:14:10 +0500

    get message logic done

--- frontend/src/components/MessageContainer.jsx
+++ frontend/src/components/MessageContainer.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
 import SendInput from './SendInput'
 import Messages from './Messages'
+import { useSelector } from 'react-redux';
 
 const MessageContainer = () => {
+
+    const { selectedUser } = useSelector(store => store.user);
+
     return (
         <div className='md:min-w-[550px] flex flex-col'>
 
             <div className="flex gap-2 items-center bg-zinc-800 text-white px-4 py-2 mb-2">
                 <div className={`avatar online`}>
                     <div className='w-12 rounded-full'>
-                        <img src={"https://avatar.iran.liara.run/public/boy?username=haram123"} alt="user-profile" />
+                        <img src={selectedUser?.profilePhoto} alt="user-profile" />
                     </div>
                 </div>
 
                 <div className='flex flex-col flex-1'>
                     <div className='flex justify-between gap-2'>
-                        <p>haram</p>
+                        <p>{selectedUser?.fullName}</p>
                     </div>
                 </div>
 
