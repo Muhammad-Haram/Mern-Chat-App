464ce58830d732ae6c573357c336f25b9e59bded
Muhammad-Haram <haramh643@gmail.com>
2024-11-29 14:45:47 +0500

    edit

--- frontend/src/components/SendInput.jsx
+++ frontend/src/components/SendInput.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { SendHorizontal } from 'lucide-react';
+import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
 
 const SendInput = () => {
+
+    const [input, setInput] = useState("")
+    const onSubmitHandler = async (e) => {
+        e.preventDefault();
+
+        try {
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
-        <form className='px-4 my-3'>
+        <form onSubmit={onSubmitHandler} className='px-4 my-3'>
             <div className='w-full relative'>
                 <input
                     type="text"
                     placeholder='Send a message...'
                     className='border text-sm rounded-lg block w-full p-3 border-zinc-500 bg-gray-600 text-white'
+                    onChange={(e) => { setInput(e.target.value) }}
+                    value={input}
                 />
                 <button type="submit" className='absolute flex inset-y-0 end-0 items-center pr-4'>
                     <SendHorizontal />
