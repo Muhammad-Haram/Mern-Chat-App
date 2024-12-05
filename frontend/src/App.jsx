7a86c863c495004a259f584d94a3a2d176e4e63b
Muhammad Haram <haramh643@gmail.com>
2024-12-06 02:40:33 +0500

    socket io implement

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import HomePage from './components/HomePage'
 import Signup from './components/Signup'
 import Login from './components/Login'
+import { useDispatch, useSelector } from 'react-redux'
+import io from "socket.io-client"
 
 const router = createBrowserRouter([
   {
@@ -21,6 +23,20 @@ const router = createBrowserRouter([
 ])
 
 const App = () => {
+
+  const { authUser } = useSelector(store => store.user);
+  const dispatch = useDispatch();
+  const [socket, setSocket] = useState(null)
+
+  useEffect(() => {
+    if (authUser) {
+      const socket = io("http://localhost:8080/", {
+
+      });
+      setSocket(socket)
+    }
+  }, [authUser])
+
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
       <RouterProvider router={router} />
